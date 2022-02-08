19bf80d14480733971f290c6c5375f90ae1ae2d4
NealLyonsWake <waken.games@gmail.com>
2022-02-08 18:28:54 +0000

    Overhaul of /recommend route code

--- routes/index.js
+++ routes/index.js
@@ -5,45 +5,72 @@ const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fet
 
 const apiKey = process.env.API_KEY
 
-let IMAGE_BASE
-let IMAGE_SIZE
-let pageNumber = Math.floor(Math.random() * 500) + 1
+// let IMAGE_BASE
+// let IMAGE_SIZE
+// let pageNumber = Math.floor(Math.random() * 500) + 1
 
-async function config() {
-  const configReply = await fetch(`https://api.themoviedb.org/3/configuration?api_key=${apiKey}`,
-    { method: 'GET' }
-    
-  )
-  const configReplyJSON = await configReply.json()
-  const images = configReplyJSON.images
-  const { base_url, poster_sizes } = images
-  IMAGE_BASE = base_url
-  IMAGE_SIZE = poster_sizes[2]
-}
+// async function config() {
+//   const configReply = await fetch(`https://api.themoviedb.org/3/configuration?api_key=${apiKey}`,
+//     { method: 'GET' }
+
+//   )
+//   const configReplyJSON = await configReply.json()
+//   const images = configReplyJSON.images
+//   const { base_url, poster_sizes } = images
+//   IMAGE_BASE = base_url
+//   IMAGE_SIZE = poster_sizes[2]
+// }
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Tasty TV API' });
 });
 
+
 router.get(`/recommend`, async function (req, res) {
-  await config()
-  const movieReply = await fetch(
-    `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&language=en-US&sort_by=popularity.desc&certification_country=GB&certification.lte=18&include_adult=false&include_video=false&page=${pageNumber}&with_watch_monetization_types=flatrate`,
-    { method: 'GET' }
-  );
-  const movieReplyJSON = await movieReply.json();
-  const movies = movieReplyJSON.results;
-  const movieSend = movies.map((item) => {
-    return {
-      id: item.id,
-      title: item.title,
-      posterURL: `${IMAGE_BASE}${IMAGE_SIZE}${item.poster_path}`,
-      overview: item.overview
-    }
-  })
-  pageNumber = Math.floor(Math.random() * 500) + 1;
-  res.send(movieSend)
+  // await config()
+
+  let IMAGE_BASE
+  let IMAGE_SIZE
+  let pageNumber = Math.floor(Math.random() * 500) + 1
+
+  try {
+
+    const configReply = await fetch(`https://api.themoviedb.org/3/configuration?api_key=${apiKey}`,
+      { method: 'GET' }
+    )
+
+    const configReplyJSON = await configReply.json()
+    const images = configReplyJSON.images
+    const { base_url, poster_sizes } = images
+    IMAGE_BASE = base_url
+    IMAGE_SIZE = poster_sizes[2]
+
+
+    const movieReply = await fetch(
+      `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&language=en-US&sort_by=popularity.desc&certification_country=GB&certification.lte=18&include_adult=false&include_video=false&page=${pageNumber}&with_watch_monetization_types=flatrate`,
+      { method: 'GET' }
+    );
+
+    const movieReplyJSON = await movieReply.json();
+    const movies = movieReplyJSON.results;
+    const movieSend = movies.map((item) => {
+      return {
+        id: item.id,
+        title: item.title,
+        posterURL: `${IMAGE_BASE}${IMAGE_SIZE}${item.poster_path}`,
+        overview: item.overview
+      }
+    })
+    pageNumber = Math.floor(Math.random() * 500) + 1;
+    res.send(movieSend)
+  }
+
+  catch (e) {
+    console.log(e)
+    res.json({ message: 'Call to movie API failed' })
+  }
+  
 });
 
 
